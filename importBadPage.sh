71016eb3581cc7ae736f3a6df7db064af0416e44
Chris Tweney <chris@media.berkeley.edu>
2012-04-16 17:37:01 -0700

    Change bad page to different data

--- importBadPage.sh
+++ importBadPage.sh
@@ -3,6 +3,6 @@
 # strip newlines
 JSON=`cat PageWithUndefinedID.json | tr -d '\n'`
 
-curl -e "/dev/test" -u chris:test "http://localhost:8080/p/lsGZ8gIYaa" -F":operation=import" \
+curl -e "/dev/test" -u chris:test "http://localhost:8080/p/l6CBMyCsaa" -F":operation=import" \
     -F:content="$JSON" -F":replaceProperties=true" -F":replace=true" -F":contentType=json"
 
