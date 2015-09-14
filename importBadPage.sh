ac0f99e84c490bf3b45e24c757220e430f86e658
Chris Tweney <chris@media.berkeley.edu>
2015-09-14 16:22:01 -0700

    Remove a bunch of obsolete OAE-era crud

--- importBadPage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# strip newlines
-JSON=`cat PageWithUndefinedID.json | tr -d '\n'`
-
-curl -e "/dev/test" -u chris:test "http://localhost:8080/p/l6CBMyCsaa" -F":operation=import" \
-    -F:content="$JSON" -F":replaceProperties=true" -F":replace=true" -F":contentType=json"
-
